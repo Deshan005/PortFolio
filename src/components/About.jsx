a47ba89509790477622bb057b55fb75206fa02bb
Deshan005 <deshanw77@gmail.com>
2023-07-23 18:15:40 +0530

    commiting 1st time

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20 text-justify">
+          I'am Namidu Deshan Wijethunga. Currently, I am in my second year of studies, and I have been actively involved in 
+          enhancing my technical skills and knowledge. In addition to my coursework, I have been 
+          dedicating my time to self-study the MERN stack, which encompasses MongoDB, Express.js,
+          React.js, and Node.js. I believe that by learning this stack, I am equipping myself to 
+          become a well-rounded and versatile developer.
+        </p>
+
+        <br />
+
+        <p className="text-xl text-justify">
+        Apart from my technical abilities, I possess strong leadership qualities, as 
+        demonstrated by my involvement in various group projects and extracurricular activities 
+        during my academic journey. I believe in effective communication, collaboration, and the ability to inspire and motivate team members to achieve common goals.
+        One of my strengths is my commitment to meeting deadlines and delivering quality work. 
+        I understand the importance of responsibility and accountability in a professional 
+        environment. I take ownership of my tasks and strive to exceed expectations by 
+        producing high-quality code and solutions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import hangman from "../assets/portfolio/hangman.png";
+import fuel from "../assets/portfolio/fuel.png";
+import progression from "../assets/portfolio/progression.png";
+import portfolio from "../assets/portfolio/portfolio.png";
+import skincare from "../assets/portfolio/skincare.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: hangman,
+      link: "https://github.com/Deshan005/Hangman-Game",
+    },
+    {
+      id: 2,
+      src: fuel,
+      link: "https://github.com/Deshan005/Fuel-Queue-Management-System",
+    },
+    {
+      id: 3,
+      src: progression,
+      link: "https://github.com/Deshan005/Progression-Outcome-Predictor",
+    },
+    {
+      id: 4,
+      src: portfolio,
+      link: "",
+    },
+    {
+      id: 5,
+      src: skincare,
+      link: "https://github.com/Deshan005/Skin-Consultation-Management-System",
+    },
+  ];
+
+  const handleButtonClick = (link, event) => {
+    event.preventDefault();
+    if (link) {
+      window.open(link);
+    }
+  };
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:p-0">
+          {portfolios.map(({ id, src, link }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-500 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={(event) => handleButtonClick(link, event)}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  <a href={link}>Code</a>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
