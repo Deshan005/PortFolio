164a4d7bdb3db032d5ffb4eba57c386eb013879c
Deshan005 <deshanw77@gmail.com>
2023-07-27 14:34:17 +0530

    sociallink changed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -9,7 +9,7 @@ const SocialLinks = () => {
       id: 1,
       child: (
         <>
-          LinkedIn <FaLinkedin size={25} />
+          LinkedIn <FaLinkedin size={30} />
         </>
       ),
       href: "https://linkedin.com",
@@ -19,28 +19,28 @@ const SocialLinks = () => {
       id: 2,
       child: (
         <>
-          GitHub <FaGithub size={25} />
+          GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/Deshan005",
+      href: "https://github.com/theyashpatel",
     },
     {
       id: 3,
       child: (
         <>
-          Mail <HiOutlineMail size={25} />
+          Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:deshanw77@gmail.com",
+      href: "mailto:foo@gmail.com",
     },
     {
       id: 4,
       child: (
         <>
-          Resume <BsFillPersonLinesFill size={25} />
+          Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Deshan_CV.pdf",
+      href: "/resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
@@ -53,7 +53,7 @@ const SocialLinks = () => {
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 " +
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
               " " +
               style
             }
