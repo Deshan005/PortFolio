e0cec92f0c164129709c413fb6813083b40a3d7f
Deshan Wijethunga <deshanw77@gmail.com>
2023-08-23 01:28:11 +0530

    updated the href link to CV

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/public/CV-DESHAN.pdf",
       style: "rounded-br-md",
       download: true,
     },
