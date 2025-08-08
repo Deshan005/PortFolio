2c5627ce653d23da77e8250d1728a26ef1f2e4b9
Deshan005 <deshanw77@gmail.com>
2025-08-08 21:20:25 +0530

    updated my details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   return (
@@ -9,23 +10,36 @@ const Home = () => {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-between h-full px-4">
+        {/* Left: Text */}
+        <div className="flex flex-col justify-center h-full md:w-1/2">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Web Developer
+            I'm a{" "}
+            <span className="text-cyan-400">
+              <Typewriter
+                words={["Software Engineer", "Web Developer", "Full Stack Developer"]}
+                loop={true}
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={2000}
+              />
+            </span>
           </h2>
+
           <p className="text-gray-500 py-4 max-w-md text-justify">
-            I am Currently an Undergraduate student who pursue a degree in 
-            Software Engineering. Currently working on MERN stack with a goal of
-            becoming a full stack developer.
+            I am currently an undergraduate student pursuing a degree in
+            Software Engineering. I'm actively working on the MERN stack with
+            the goal of becoming a full-stack developer.
           </p>
 
-          <div>
+          <div className="mt-8">
             <Link
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
@@ -35,11 +49,11 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <div className="md:w-1/2">
           <img
             src={HeroImage}
             alt="profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-40 md:w-72"
           />
         </div>
       </div>
