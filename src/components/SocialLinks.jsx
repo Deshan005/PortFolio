1624931c102cbb1d9728dbdf4c5442cdc3b9386d
Deshan005 <deshanw77@gmail.com>
2025-09-27 17:19:06 +0530

    added a ico icon

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -44,7 +44,7 @@ const SocialLinks = () => {
               key={id}
               className={
                 `flex justify-between items-center w-40 h-14 px-4 ml-[-100px] 
-                hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 text-white` +
+                hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-700 text-white` +
                 ` ${style}`
               }
             >
